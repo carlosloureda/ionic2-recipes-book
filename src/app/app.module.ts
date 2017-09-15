b4fbeb5ea97533b337f9c378b2f621bbfa2a5c97
Carlos Loureda <carlos.loureda@domesting.com>
2017-09-15 20:28:12 +0200

    Added template driven from to shopping list view (basic)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,9 @@
+import { ShoppingListService } from './../services/shopping-list';
+import { TabsPage } from './../pages/tabs/tabs';
+import { ShoppingListPage } from './../pages/shopping-list/shopping-list';
+import { RecipesPage } from './../pages/recipes/recipes';
+import { EditRecipePage } from './../pages/edit-recipe/edit-recipe';
+import { RecipePage } from './../pages/recipe/recipe';
 import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
@@ -5,12 +11,15 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    RecipePage,
+    RecipesPage,
+    EditRecipePage,
+    ShoppingListPage,
+    TabsPage
   ],
   imports: [
     BrowserModule,
@@ -19,12 +28,17 @@ import { HomePage } from '../pages/home/home';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    RecipePage,
+    RecipesPage,
+    EditRecipePage,
+    ShoppingListPage,
+    TabsPage
   ],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    ShoppingListService
   ]
 })
 export class AppModule {}
---
+++ src/pages/shopping-list/shopping-list.ts
@@ -0,0 +1,44 @@
+import { Ingredient } from './../../models/ingredient';
+import { ShoppingListService } from './../../services/shopping-list';
+import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+
+/**
+ * Template driven form
+ * */
+
+@IonicPage()
+@Component({
+  selector: 'page-shopping-list',
+  templateUrl: 'shopping-list.html',
+})
+
+export class ShoppingListPage {
+
+  private listItems: Ingredient[];
+  constructor(private slService: ShoppingListService) {}
+
+  onAddItem(form: NgForm) {
+    console.log("on item added");
+    // console.log(form);
+    this.slService.addItem(form.value.ingredientName, form.value.amount);
+    form.reset();
+    // refresh the list
+    this.loadItems(); 
+  }
+  // will be fired even popon cached views
+  ionViewWillEnter() {
+    this.loadItems();
+  }
+
+  onCheckedItem(index: number) {
+    this.slService.removeItem(index);
+    this.loadItems();
+  }
+
+  private loadItems() {
+    this.listItems = this.slService.getItems();
+  }
+
+}
